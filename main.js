25d03efc49ce0997b852cf727168fa44ab2220a9
carolourdp97@gmail.com <carolourdp97@gmail.com>
2023-11-14 17:18:08 -0300

    web

---
+++ main.js
@@ -0,0 +1,217 @@
+function sound1() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND1.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound2() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND104.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound3() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND105.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound4() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND108.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound5() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND111.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound6() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND112.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound7() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND12.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound8() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND13.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound9() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND136.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound10() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND131.WAV").toDestination();
+  player.autostart = true;
+  }
+  function sound11() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND14.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound12() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND16.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound13() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND17.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound11() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND18.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound11() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND19.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound11() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND20.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound14() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND21.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound15() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND22.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound16() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND24.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound17() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND25.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound18() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118efa4fbe27b28d5ebeba383447fc/SOUND26.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound19() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND27.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound20() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND28.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound21() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND29.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound22() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND30.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound23() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND34.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound24() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND35.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound25() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND35.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  function sound26() {
+    const player = new Tone.Player("https://raw.githubusercontent.com/dipaola-c/2.1-sonido-y-color-1/e9cd6b032118e9fa4fbe27b28d5ebeba383447fc/SOUND36.WAV").toDestination();
+  player.autostart = true;
+  }
+  
+  
+  
+  
+  Mousetrap.bind(['q'], function() {sound1(); 
+                                });
+  
+  Mousetrap.bind(['w'], function() {sound2(); 
+                                });
+  
+  Mousetrap.bind(['e'], function() {sound3(); 
+                                });
+  
+  Mousetrap.bind(['r'], function() {sound4(); 
+                                });
+  
+  Mousetrap.bind(['t'], function() {sound5(); 
+                                });
+  
+  Mousetrap.bind(['y'], function() {sound6(); 
+                                });
+  
+  Mousetrap.bind(['u'], function() {sound7(); 
+                                });
+  
+  Mousetrap.bind(['i'], function() {sound8(); 
+                                });
+  
+  Mousetrap.bind(['o'], function() {sound9(); 
+                                });
+  
+  Mousetrap.bind(['p'], function() {sound10(); 
+                                });
+  
+  Mousetrap.bind(['a'], function() {sound11(); 
+                                });
+  
+  Mousetrap.bind(['s'], function() {sound12(); 
+                                });
+  
+  Mousetrap.bind(['d'], function() {sound13(); 
+                                });
+  
+  Mousetrap.bind(['f'], function() {sound14(); 
+                                });
+  
+  Mousetrap.bind(['g'], function() {sound15(); 
+                                });
+  
+  Mousetrap.bind(['h'], function() {sound16(); 
+                                });
+  
+  Mousetrap.bind(['j'], function() {sound17(); 
+                                });
+  
+  Mousetrap.bind(['k'], function() {sound18(); 
+                                });
+  
+  Mousetrap.bind(['l'], function() {sound19(); 
+                                });
+  
+  Mousetrap.bind(['z'], function() {sound20(); 
+                                });
+  
+  Mousetrap.bind(['x'], function() {sound21(); 
+                                });
+  
+  Mousetrap.bind(['c'], function() {sound22(); 
+                                });
+  
+  Mousetrap.bind(['v'], function() {sound23(); 
+                                });
+  
+  Mousetrap.bind(['b'], function() {sound24(); 
+                                });
+  
+  Mousetrap.bind(['n'], function() {sound25(); 
+                                });
+  
+  Mousetrap.bind(['m'], function() {sound26(); 
+                                });
+  
